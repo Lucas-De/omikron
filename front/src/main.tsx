900d022559e930e38583730aee9ea5b278da4498
Lucas Descause <lucas.descause@gmail.com>
2024-04-02 09:25:06 -0400

    routes

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,9 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { LoginPage } from "./modules/login/pages/LoginPage";
+import { HomePage } from "./modules/home/pages/HomePage";
+import "./modules/common/app.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LoginPage />,
+    errorElement: <div>Not found</div>,
+  },
+  {
+    path: "/home",
+    element: <HomePage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
---
+++ front/src/modules/login/pages/LoginPage.tsx
@@ -0,0 +1,16 @@
+export function LoginPage() {
+  return (
+    <>
+      <div className="center-content">Your centered content here</div>
+
+      <style jsx>{`
+        .center-content {
+          color: red;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+      `}</style>
+    </>
+  );
+}
