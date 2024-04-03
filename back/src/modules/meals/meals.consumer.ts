4e466c2069e3bc4abd7a9970d21dd4c771556287
Lucas Descause <lucas.descause@gmail.com>
2024-04-03 00:44:45 -0400

    openAi integration

--- back/src/modules/meals/meals.consumer.ts
+++ back/src/modules/meals/meals.consumer.ts
@@ -1,10 +1,19 @@
 import { connect, Channel, Connection, ConsumeMessage } from 'amqplib';
 import { MealsService } from './meals.service';
+import OpenAI from 'openai';
+
+interface EstimatedMealStats {
+  fatGrams: number;
+  carbGrams: number;
+  proteinGrams: number;
+  calories: number;
+}
 
 export class AnalyzedMealConsumer {
   private readonly MEAL_QUEUE_NAME = 'meal-queue';
   private connection: Connection;
   private channel: Channel;
+  private openai: OpenAI;
 
   constructor(private readonly mealsService: MealsService) {}
 
@@ -17,6 +26,7 @@ export class AnalyzedMealConsumer {
         password: 'password',
       });
       this.channel = await this.connection.createChannel();
+      this.openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
       this.startConsuming();
     } catch (error) {
       console.error('Failed to connect to RabbitMQ', error);
@@ -28,14 +38,36 @@ export class AnalyzedMealConsumer {
     this.channel.consume(this.MEAL_QUEUE_NAME, this.consume);
   }
 
-  consume = async (message: ConsumeMessage) => {
-    const { id, ...nutritionalInfo } = JSON.parse(message.content.toString());
-    await this.mealsService.update(id, {
-      ...nutritionalInfo,
-      proteins: 200,
-      calories: 1450,
+  async getMealStats(description: string): Promise<EstimatedMealStats> {
+    const prompt = `Answer only with a JSON that matches \
+    {proteinGrams:number,fatGrams:number,carbGrams:number,calories:number}\
+    Estimate the macronutrients and calories of the following meal "${description}\
+    If not a meal, answer with JSON {error:true}`;
+
+    const { choices } = await this.openai.chat.completions.create({
+      response_format: { type: 'json_object' },
+      messages: [{ role: 'system', content: prompt }],
+      model: 'gpt-3.5-turbo',
     });
-    this.channel.ack(message);
+    console.log(choices[0].message.content);
+    return JSON.parse(choices[0].message.content);
+  }
+
+  consume = async (message: ConsumeMessage) => {
+    try {
+      const { id, description } = JSON.parse(message.content.toString());
+      const mealStats = await this.getMealStats(description);
+      await this.mealsService.update(id, {
+        fats: mealStats?.fatGrams,
+        carbs: mealStats?.carbGrams,
+        proteins: mealStats?.proteinGrams,
+        calories: mealStats?.calories,
+      });
+    } catch (err) {
+      console.log(err);
+    } finally {
+      this.channel.ack(message);
+    }
   };
 }
 
---
+++ front/src/modules/meals/components/CaloriesLineChart.tsx
@@ -0,0 +1,59 @@
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  maintainAspectRatio: false,
+  plugins: {
+    legend: {
+      display: false,
+    },
+    title: {
+      display: false,
+    },
+  },
+};
+
+const labels = ["January", "February", "March", "April", "May", "June", "July"];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: "Dataset 1",
+      data: [12, 12, 14, 188, 122, 12, 199],
+      borderColor: "rgb(255, 99, 132)",
+      backgroundColor: "rgba(255, 99, 132, 0.5)",
+      cubicInterpolationMode: "monotone",
+    },
+  ],
+};
+
+export function CaloriesLineChart() {
+  return (
+    <Line
+      options={options}
+      data={data}
+      style={{ height: 275, width: "100%" }}
+    />
+  );
+}
