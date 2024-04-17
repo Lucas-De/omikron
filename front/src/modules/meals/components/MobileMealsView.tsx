8379539333cebadc2f6fcb96b0573ac689aaf132
Lucas Descause <lucas.descause@gmail.com>
2024-04-17 11:53:07 -0400

    add mobile logout

--- front/src/modules/meals/components/MobileMealsView.tsx
+++ front/src/modules/meals/components/MobileMealsView.tsx
@@ -1,11 +1,14 @@
-import { Space } from "antd";
+import { Button, Space } from "antd";
 import { useMealsStore } from "../meals.store";
 import { useEffect } from "react";
 import { MobileMealCard } from "./MobileMealCard";
 import { MobilePage } from "../../../common/components/MobilePage";
 import { MealCreationButton } from "./MealCreationButton";
+import { LogoutOutlined } from "@ant-design/icons";
+import { useAuthenticationStore } from "../../authentication/authentication.store";
 
 export function MobileMealsView() {
+  const logout = useAuthenticationStore((state) => state.logout);
   const listMeals = useMealsStore((state) => state.listMeals);
   const loading = useMealsStore((state) => state.loading);
   const meals = useMealsStore((state) => state.meals);
@@ -13,11 +16,25 @@ export function MobileMealsView() {
     listMeals();
   }, []);
 
-  const actionCorner = <MealCreationButton type="mobile" />;
+  const addButton = <MealCreationButton type="mobile" />;
+  const logoutButton = (
+    <Button
+      onClick={logout}
+      size="middle"
+      type="text"
+      shape="circle"
+      icon={<LogoutOutlined />}
+    />
+  );
 
   return (
     <>
-      <MobilePage loading={loading} title="Meals" actionCorner={actionCorner}>
+      <MobilePage
+        loading={loading}
+        title="Meals"
+        leftCorner={addButton}
+        rightCorner={logoutButton}
+      >
         <Space direction="vertical" size="middle">
           {meals.map((meal) => (
             <MobileMealCard key={meal.id} meal={meal} />
