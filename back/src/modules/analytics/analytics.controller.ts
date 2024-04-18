7dd1eebb00d7dcec0202771232418eb0226fdf19
Lucas Descause <lucas.descause@gmail.com>
2024-04-18 08:59:30 -0400

    add lookBackDays query for analytics

--- back/src/modules/analytics/analytics.controller.ts
+++ back/src/modules/analytics/analytics.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AnalyticsService } from './analytics.service';
+import { AnalyticsFilterQuery } from './dto/analytics-filter-query.dto';
 
 @Controller('/users/:userId/analytics')
 export class AnalyticsController {
   constructor(private readonly analyticsService: AnalyticsService) {}
 
   @Get('/meals')
-  getMealStats(@Param('userId') userId: number) {
-    return this.analyticsService.getMealStats(userId);
+  getMealStats(
+    @Param('userId') userId: number,
+    @Query() query: AnalyticsFilterQuery,
+  ) {
+    return this.analyticsService.getMealStats(userId, query.lookBackDays);
   }
 }
--- front/src/modules/analytics/analytics.service.ts
+++ front/src/modules/analytics/analytics.service.ts
@@ -5,6 +5,7 @@ export const analyticsService = {
   getDateNutrientCounts(userId: number): Promise<DateNutrientCount[]> {
     return httpRequest({
       path: `/users/${userId}/analytics/meals`,
+      query: { lookBackDays: 1 },
       method: "GET",
     });
   },
