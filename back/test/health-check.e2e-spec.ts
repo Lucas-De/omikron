ae0e667994018ff633cf5e1a08a5ab95450c413f
Lucas Descause <lucas.descause@gmail.com>
2024-04-26 15:16:27 -0400

    fix e2e test

--- back/test/health-check.e2e-spec.ts
+++ back/test/health-check.e2e-spec.ts
@@ -1,14 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from '../src/modules/app.module';
+import { HealthCheckModule } from '../src/modules/health-check/health-check.module';
 
-describe('App', () => {
+describe('Healthcheck Module', () => {
   let app: INestApplication;
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [HealthCheckModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
